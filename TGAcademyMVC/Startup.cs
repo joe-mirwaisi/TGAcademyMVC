7a9115c753b677cabc37042ca446d5eace61c890
Joe Mirwaisi <jmirwaisi@techtonicgroup.com>
2018-03-01 15:46:44 -0700

    Initial OAuth Nuget packages added and stub function placed in Startup.Cs

--- TGAcademyMVC/Startup.cs
+++ TGAcademyMVC/Startup.cs
@@ -12,6 +12,14 @@ using Microsoft.Extensions.DependencyInjection;
 using TGAcademyMVC.Data;
 using TGAcademyMVC.Models;
 using TGAcademyMVC.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
 
 namespace TGAcademyMVC
 {
@@ -20,7 +28,6 @@ namespace TGAcademyMVC
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            
         }
 
         public IConfiguration Configuration { get; }
@@ -44,6 +51,50 @@ namespace TGAcademyMVC
             services.AddTransient<IEmailSender, EmailSender>();
 
             services.AddMvc();
+
+            AuthenticationBuilder ab = new AuthenticationBuilder(services);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = "GitHub";
+            })
+       .AddCookie()
+       .AddOAuth("GitHub", options =>
+       {
+           options.ClientId = "ced302c68a0daca68574";
+           options.ClientSecret = "9bf9fe57e09c7b7d6511e98602df5c2b187eca36";
+           options.CallbackPath = new PathString("/signin-github");
+
+           options.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+           options.TokenEndpoint = "https://github.com/login/oauth/access_token";
+           options.UserInformationEndpoint = "https://api.github.com/user";
+
+           options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+           options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+           options.ClaimActions.MapJsonKey("urn:github:login", "login");
+           options.ClaimActions.MapJsonKey("urn:github:url", "html_url");
+           options.ClaimActions.MapJsonKey("urn:github:avatar", "avatar_url");
+
+           options.Events = new OAuthEvents
+           {
+               OnCreatingTicket = async context =>
+               {
+                   var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+                   request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                   request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+
+                   var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+                   response.EnsureSuccessStatusCode();
+
+                   var user = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+                   context.RunClaimActions(user);
+               }
+           };
+       });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
