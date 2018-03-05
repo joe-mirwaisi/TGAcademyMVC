142761e96c0bfb9e665bf0a7b02f45249b217e29
Joe Mirwaisi <jmirwaisi@techtonicgroup.com>
2018-03-05 16:43:44 -0700

    Unit Tests setup to allow for CreateRole function to be invoked

--- TGAcademyMVC/Startup.cs
+++ TGAcademyMVC/Startup.cs
@@ -1,25 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
 using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
+using Newtonsoft.Json.Linq;
+
 using TGAcademyMVC.Data;
 using TGAcademyMVC.Models;
 using TGAcademyMVC.Services;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Http;
-using System.Security.Claims;
-using Microsoft.AspNetCore.Authentication.OAuth;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using Newtonsoft.Json.Linq;
 
 namespace TGAcademyMVC
 {
@@ -60,8 +63,8 @@ namespace TGAcademyMVC
                 options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = "GitHub";
             })
-       .AddCookie()
-       .AddOAuth("GitHub", options =>
+            .AddCookie()
+            .AddOAuth("GitHub", options =>
        {
            options.ClientId = "ced302c68a0daca68574";
            options.ClientSecret = "9bf9fe57e09c7b7d6511e98602df5c2b187eca36";
@@ -93,8 +96,10 @@ namespace TGAcademyMVC
                    context.RunClaimActions(user);
                }
            };
-       });
 
+        });
+
+            Startup.CreateRoles(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -121,6 +126,56 @@ namespace TGAcademyMVC
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        public static void CreateRoles(IServiceCollection services)
+        {
+            var serviceProvider = services.BuildServiceProvider();
+
+            //initializing custom roles 
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            string[] roleNames = { "Prospective_Student", "Current_Student", "Mentor", "Admin", };
+            Task<IdentityResult> roleResult;
+
+            foreach (var roleName in roleNames)
+            {
+                Task<bool> roleExist = roleManager.RoleExistsAsync(roleName);
+                roleExist.Wait();
+                if (!roleExist.Result)
+                {
+                    //create the roles and seed them to the database: Question 1
+                    roleResult = roleManager.CreateAsync(new IdentityRole(roleName));
+                    roleResult.Wait();
+                }
+            }
+
+            ApplicationUser[] testUsers = {
+            new ApplicationUser { UserName = "test_ps", Email = "test_ps@techtonicgroup.com", },
+            new ApplicationUser { UserName = "test_cs", Email = "test_cs@techtonicgroup.com", },
+            new ApplicationUser { UserName = "test_mentor", Email = "test_mentor@techtonicgroup.com", },
+            new ApplicationUser { UserName = "test_admin", Email = "test_admin@techtonicgroup.com", }
+        };
+
+            //Ensure you have these values in your appsettings.json file
+            string userPWD = "Password123!";
+            for (int i = 0; i < testUsers.Length; i++)
+            {
+                Task<ApplicationUser> user = userManager.FindByEmailAsync(testUsers[i].Email);
+                user.Wait();
+                if (user.Result == null)
+                {
+                    var newTestUser = userManager.CreateAsync(testUsers[i], userPWD);
+                    newTestUser.Wait();
+                }
+                else
+                {
+                    testUsers[i].Id = user.Result.Id;
+                }
+
+                var assignToRole = userManager.AddToRoleAsync(testUsers[i], roleNames[i]);
+                assignToRole.Wait();
+            }
         }
     }
 }
--- TGAcademyMVC_UnitTests/UnitTest1.cs
+++ TGAcademyMVC_UnitTests/UnitTest1.cs
@@ -1,5 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+using TGAcademyMVC;
+using TGAcademyMVC.Data;
+using TGAcademyMVC.Models;
 
 namespace TGAcademyMVC_UnitTests
 {
@@ -9,6 +16,15 @@ namespace TGAcademyMVC_UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+            ServiceCollection services = new ServiceCollection();
+            services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-TGAcademyMVC-895368D8-AF00-429F-A46B-A1F3FAE043A4;Trusted_Connection=True;MultipleActiveResultSets=true"));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            Startup.CreateRoles(services);
         }
     }
 }
